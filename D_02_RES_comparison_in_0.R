2844dee066489186f95b562e5f1c3cf7b368d311
Francesco Denti <francescodenti.personal@gmail.com>
2025-06-12 18:33:35 +0200

    Update D_02_RES_comparison_in_0.R

--- D_02_RES_comparison_in_0.R
+++ D_02_RES_comparison_in_0.R
@@ -3,7 +3,7 @@ library(tidyverse)
 
 
 
-NSAMPLE <- paste("N =",c(500,1000,5000,10000, 25000))
+NSAMPLE <- paste("N =",c(5000,10000,20000,50000))
 PROBS_BOTEV <- readRDS("RDS/Comparison_in_0/rho05_BOTEV_log2P_256.RDS")
 PROBS_GENZ  <- readRDS("RDS/Comparison_in_0/rho05_TLRNK_log2P_256.RDS")
 PROBS_RIDGE <- readRDS("RDS/Comparison_in_0/rho05_RIDGE_log2P_256.RDS")
@@ -14,14 +14,12 @@ a1 = PROBS_BOTEV %>% reshape2::melt() %>% mutate(algo = "Botev")
 a2 = PROBS_GENZ %>% reshape2::melt() %>% mutate(algo = "Genz/TLRank")
 a3 = PROBS_RIDGE %>% reshape2::melt() %>% mutate(algo = "Ridgeway")
 
-A_rho <- a1 %>% bind_rows(a2,a3) %>% mutate(cov = "Const.")
+A_rho <- a1 %>% bind_rows(a2,a3) %>% mutate(cov = "Case (ii) with rho == 0.5")
 ep_rho <- readRDS("RDS/Comparison_in_0/rho05_EPCHOL2_log2P_256.RDS")
 
 
 # -------------------------------------------------------------------------
 
-
-NSAMPLE <- paste("N =",c(500,1000,5000,10000, 25000))
 PROBS_BOTEV <- readRDS("RDS/Comparison_in_0/fung_BOTEV_log2P_256.RDS")
 PROBS_GENZ  <- readRDS("RDS/Comparison_in_0/fung_GENZ_log2P_256.RDS")
 PROBS_RIDGE <- readRDS("RDS/Comparison_in_0/fung_RIDGE_log2P_256.RDS")
@@ -32,13 +30,12 @@ a1 = PROBS_BOTEV %>% reshape2::melt() %>% mutate(algo = "Botev")
 a2 = PROBS_GENZ %>% reshape2::melt() %>% mutate(algo = "Genz/TLRank")
 a3 = PROBS_RIDGE %>% reshape2::melt() %>% mutate(algo = "Ridgeway")
 
-A_fung <- a1 %>% bind_rows(a2,a3) %>% mutate(cov = "Fung")
+A_fung <- a1 %>% bind_rows(a2,a3) %>% mutate(cov = "Fungible")
 ep_fung <- readRDS("RDS/Comparison_in_0/fung_EPCHOL2_log2P_256.RDS")
 
 
 # -------------------------------------------------------------------------
 
-NSAMPLE <- paste("N =",c(500,1000,5000,10000, 25000))
 PROBS_BOTEV <- readRDS("RDS/Comparison_in_0/Dense_BOTEV_log2P_256.RDS")
 PROBS_GENZ  <- readRDS("RDS/Comparison_in_0/Dense_GENZ_log2P_256.RDS")
 PROBS_RIDGE <- readRDS("RDS/Comparison_in_0/Dense_RIDGE_log2P_256.RDS")
@@ -63,9 +60,11 @@ colnames(A)
 
 ggplot()+
   geom_boxplot(data=A,aes(x=Var2,y=value,col=algo))+
-  facet_wrap(~cov, scale="free_y")+
+  facet_wrap(~cov, scale="free_y", label = label_parsed)+
   theme_bw()+
-  geom_hline(data=EP, aes(yintercept = ep))
+  theme(legend.position = "bottom", text = element_text(size=16))+
+  geom_hline(data=EP, aes(yintercept = ep))+
+  ggview::canvas(h=5,w=15)
 
 
 
@@ -75,9 +74,6 @@ ggplot()+
 library(tidyverse)
 # extract_results ---------------------------------------------------------
 
-
-
-NSAMPLE <- paste("N =",c(500,1000,5000,10000, 25000))
 TIMES_BOTEV <- readRDS("RDS/Comparison_in_0/rho05_BOTEV_times_256.RDS")
 TIMES_GENZ  <- readRDS("RDS/Comparison_in_0/rho05_TLRNK_times_256.RDS")
 TIMES_RIDGE <- readRDS("RDS/Comparison_in_0/rho05_RIDGE_times_256.RDS")
@@ -94,8 +90,6 @@ ep_rho <- readRDS("RDS/Comparison_in_0/rho05_EPCHOL2_times_256.RDS")
 
 # -------------------------------------------------------------------------
 
-
-NSAMPLE <- paste("N =",c(500,1000,5000,10000, 25000))
 TIMES_BOTEV <- readRDS("RDS/Comparison_in_0/fung_BOTEV_times_256.RDS")
 TIMES_GENZ  <- readRDS("RDS/Comparison_in_0/fung_GENZ_times_256.RDS")
 TIMES_RIDGE <- readRDS("RDS/Comparison_in_0/fung_RIDGE_times_256.RDS")
@@ -112,7 +106,6 @@ ep_fung <- readRDS("RDS/Comparison_in_0/fung_EPCHOL2_times_256.RDS")
 
 # -------------------------------------------------------------------------
 
-NSAMPLE <- paste("N =",c(500,1000,5000,10000, 25000))
 TIMES_BOTEV <- readRDS("RDS/Comparison_in_0/Dense_BOTEV_times_256.RDS")
 TIMES_GENZ  <- readRDS("RDS/Comparison_in_0/Dense_GENZ_times_256.RDS")
 TIMES_RIDGE <- readRDS("RDS/Comparison_in_0/Dense_RIDGE_times_256.RDS")
@@ -140,3 +133,4 @@ ggplot()+
   facet_wrap(~cov, scale="free_y")+scale_y_log10()+
   theme_bw()+
   geom_hline(data=EP, aes(yintercept = ep))
+
